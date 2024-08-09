62a9f901a3334855eb0c77400a8e695289267c17
SiddheshwarDalave <siddheshwardalave9119@gmail.com>
2024-08-09 18:36:54 +0530

    second commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,9 @@ function Contact() {
   return (
     <section id="contact" className="contact">
       <h2>Contact Details</h2>
-      <p><strong>PG Owner:</strong> John Doe | +1234567890</p>
-      <p><strong>PG Manager:</strong> Jane Smith | +0987654321</p>
-      <p><strong>Email:</strong> pgaccommodation@example.com</p>
+      <p><strong>PG Owner:</strong> BalaSaheb Jambhulkar | +919923930302</p>
+      <p><strong>PG Manager:</strong> Ratan Bhai | +917822979470</p>
+      <p><strong>Email:</strong> panchamipg@gmail.com</p>
     </section>
   );
 }
